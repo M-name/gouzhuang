0535cc18097c69341a22621f8c0cf38ec0b9d996
梅书凯 <msk@zjwit.net>
2022-12-15 10:29:14 +0800

    修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_HOST: '"/api/"',
-  PROXY_REQUEST : true  // 开启代理
+  PROXY_REQUEST : false  // 开启代理
 })
